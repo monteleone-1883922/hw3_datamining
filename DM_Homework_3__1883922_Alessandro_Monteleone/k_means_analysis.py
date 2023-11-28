241ea214c5cc0a43704a1d879df33b5c38c8fbc2
AleG19 <monteleone.1883922@studenti.uniroma1.it>
2023-11-28 13:55:38 +0100

    added jaccard similarity, compressed results, costs

--- DM_Homework_3__1883922_Alessandro_Monteleone/k_means_analysis.py
+++ DM_Homework_3__1883922_Alessandro_Monteleone/k_means_analysis.py
@@ -10,9 +10,11 @@ from sklearn.decomposition import PCA
 VALUES_FOR_N = [1000, 10000, 100000]
 VALUES_FOR_K = [50, 100, 200]
 REPORT_FILE = "experiment_results.json"
+COMPRESSED_REPORT_FILE = "experiment_results_compressed.json"
 COMPONENTS = 3
 PRECISION = 16
 SEED = 42
+TOLLERANCE = 2e-3
 
 
 def generate_dataset(n: int, k: int, d: int, s: float):
@@ -39,21 +41,30 @@ def generate_dataset(n: int, k: int, d: int, s: float):
 
 class Report():
 
-    def __init__(self, report_file):
-        self.kmeans = KMeans(init='k-means++', random_state=SEED)
+    def __init__(self, report_file, report_compressed_file):
+        self.kmeans = KMeans(init='k-means++', random_state=SEED, tol=TOLLERANCE)
         self.pca = PCA(random_state=SEED)
         self.report_file = report_file
+        self.report_compressed_file = report_compressed_file
         with open(report_file, "w") as report:
+            json.dump({}, report)
+        with open(report_compressed_file, "w") as report:
             json.dump({}, report)
 
     def set_pca(self, components):
         self.pca = PCA(n_components=components, random_state=SEED)
 
     def set_kmeans(self, k):
-        self.kmeans = KMeans(n_clusters=k, init='k-means++', random_state=SEED)
+        self.kmeans = KMeans(n_clusters=k, init='k-means++', random_state=SEED, tol=1e-3)
 
     def store_results(self, results):
-        with open(self.report_file, "r+") as report_file:
+        self.store_results_file(results, self.report_file)
+
+    def store_results_compressed(self, results):
+        self.store_results_file(results, self.report_compressed_file)
+
+    def store_results_file(self, results, file):
+        with open(file, "r+") as report_file:
             report = json.load(report_file)
             report_file.seek(0)
             key = self.parameters_to_key(results["parameters"])
@@ -67,10 +78,18 @@ class Report():
         return result[:-1]
 
     def experiment(self, data, parameters):
-        experiment_results = {"parameters": parameters, "n_components": self.pca.n_components}
+        k = parameters["k"]
+        n = parameters["n"]
+        correct_clusters = [[j + i for j in range(0, n * k, k)] for i in range(k)]
+        experiment_results = compressed_results = {"parameters": parameters, "n_components": self.pca.n_components}
         start_time = int(time.time() * 1000)
         self.kmeans.fit(data)
-        experiment_results["kmeans_running_time"] = int(time.time() * 1000) - start_time
+        experiment_results["kmeans_running_time"] = compressed_results["kmeans_running_time"] = int(
+            time.time() * 1000) - start_time
+        clusters = divide_clusters(self.kmeans.labels_.tolist())
+        experiment_results["kmeans_similarity"] = compressed_results["similarity"] = get_clusters_similarity(clusters,
+                                                                                                             k, n)
+        experiment_results["kmeans_cost"] = compressed_results["kmeans_cost"] = self.kmeans.inertia_
         experiment_results["kmeans_labels"] = self.kmeans.labels_.tolist()
         experiment_results["kmeans_centers"] = self.kmeans.cluster_centers_.tolist()
         start_time = int(time.time() * 1000)
@@ -83,22 +102,27 @@ class Report():
         experiment_results["kmeans_after_pca_running_time"] = int(time.time() * 1000) - start_time
         experiment_results["kmeans_pca_total_running_time"] = experiment_results["kmeans_after_pca_running_time"] + \
                                                               experiment_results["pca_running_time"]
+        clusters = divide_clusters(self.kmeans.labels_.tolist())
+        experiment_results["kmeans_similarity"] = compressed_results["similarity"] = get_clusters_similarity(clusters,
+                                                                                                             k, n)
+        experiment_results["kmeans_cost"] = compressed_results["kmeans_cost"] = self.kmeans.inertia_
         experiment_results["kmeans_pca_labels"] = self.kmeans.labels_.tolist()
         experiment_results["kmeans_pca_centers"] = self.kmeans.cluster_centers_.tolist()
 
+        self.store_results_compressed(compressed_results)
         self.store_results(experiment_results)
 
 
 def combine_parameters():
-    report = Report(REPORT_FILE)
+    report = Report(REPORT_FILE, COMPRESSED_REPORT_FILE)
     for n in VALUES_FOR_N:
         for k in VALUES_FOR_K:
             report.set_kmeans(k)
 
-            values_for_d = [k, 100 * k, 100 * k ** 2]
+            values_for_d = [k, 100 * k]
             values_for_s = [1 / k, 1 / math.sqrt(k), 0.5]
             for d in values_for_d:
-                report.set_pca((k + d) // COMPONENTS)
+                report.set_pca(k // COMPONENTS)
                 for s in values_for_s:
                     parameters = {
                         "n": n,
@@ -113,6 +137,43 @@ def combine_parameters():
                     report.experiment(data, parameters)
 
 
+def combine_results():
+    with open("experiment_results2.json") as f:
+        ex2 = json.load(f)
+
+    with open("experiment_results.json", "r") as f1:
+        ex1 = json.load(f1)
+    for key in ex2.keys():
+        ex1[key] = ex2[key]
+    with open("experiment_results.json", "w") as f2:
+        json.dump(ex1, f2)
+
+
+def divide_clusters(clusters):
+    divided_clusters = {}
+    for i in range(len(clusters)):
+        divided_clusters[clusters[i]] = divided_clusters.get(clusters[i], []) + [i]
+    return [value for value in divided_clusters.values()]
+
+
+def get_clusters_similarity(clusters, k, n):
+    total_similarity = 0
+    for i in range(k):
+        correct_cluster = [j + i for j in range(0, n * k, k)]
+        jaccard_similarity = -1
+        for cluster in clusters:
+            jaccard_similarity = max(compute_jaccard_similarity(set(correct_cluster), set(cluster)), jaccard_similarity)
+        total_similarity += jaccard_similarity
+    return total_similarity / k
+
+
+def compute_jaccard_similarity(set1, set2):
+    intersection_sets = set1.intersection(set2)
+    union_sets = set1.union(set2)
+    return len(intersection_sets) / len(union_sets)
+
+
 if __name__ == "__main__":
-    np.random.seed(42)
+    # combine_results()
+    np.random.seed(SEED)
     combine_parameters()
